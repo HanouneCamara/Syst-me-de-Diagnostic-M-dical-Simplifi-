77ca209645bf648d515032607cdc51d6205c7f2c
HanouneCamara <Alicmr13@gmail.com>
2025-04-24 16:52:29 +0100

    messageBienvenue

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 # Message de bienvenue
-print("Bienvenue dans le Super-Diagnostiqueur 3000 🤖")
+print("👋 Bienvenue dans le Super-Diagnostiqueur 0107002 🤖")
+print("Je vous aide à détecter rapidement certaines maladies simples à partir de vos symptômes. Veuillez répondre aux questions qui vont suivre.")
+print("⚠️ Ce système ne remplace pas un avis médical professionnel.")
+
+#Les questions
+print("Veuillez répondre aux questions suivantes par 'oui' ou 'non' ")
